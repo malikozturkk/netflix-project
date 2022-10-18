1f2890f2362e5787eafb4213b04d34b92d38bc50
malik.ozturk <malik.ozturk@enuygun.com>
2022-10-18 19:16:10 +0300

    feat: added movie modal templates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../hooks/useAuth'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <AuthProvider>
-    <Component {...pageProps} />
-  </AuthProvider>
+  return (
+    <RecoilRoot>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </RecoilRoot>
+  )
   
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,10 @@ import requests from '../utils/requests'
 import { Movie } from '../typings'
 import Row from "../components/Row"
 import useAuth from '../hooks/useAuth'
+import { useRecoilValue } from 'recoil'
+import { useState } from 'react'
+import { modalState } from '../atoms/modalAtom'
+import Modal from '../components/Modal'
 
 
 interface Props {
@@ -29,6 +33,8 @@ const Home = ({
   trendingNow,
 }: Props) => {
   const {logout, loading} = useAuth()
+  const showModal = useRecoilValue(modalState)
+  
   if (loading) return "Loading"
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
@@ -50,7 +56,7 @@ const Home = ({
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {/* Modal */}
+      {showModal && <Modal />}
     </div>
 
   )
