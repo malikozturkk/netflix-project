b4855d14d58515138b36b49c9214e9101d085e2d
malik.ozturk <malik.ozturk@enuygun.com>
2022-12-30 22:57:35 +0300

    fix: fixed banner img problem

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -25,7 +25,6 @@ function Banner({ netflixOriginals }: Props) {
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
         <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
             <Image 
-            fill
             sizes="initial"
             alt="Movie Image"
             src="https://image.tmdb.org/t/p/original/edOt2jNyCbb8SQSvulD2sRRJhmA.jpg" 
